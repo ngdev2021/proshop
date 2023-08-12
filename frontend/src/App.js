77522857df7156fe104aaaf1c20a68c62dd7f6fc
ngdev2021 <90414221+ngdev2021@users.noreply.github.com>
2023-08-11 20:34:33 -0400

    added Product, ProductScreen, and Rating components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Welcome to Pro Shop</h1>
-    </div>
+  return (<>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
+    <Footer />
+</>
   );
 }
 
