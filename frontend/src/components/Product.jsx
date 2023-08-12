677869f4914e932f435799f641c423affcb86312
ngdev2021 <90414221+ngdev2021@users.noreply.github.com>
2023-08-11 22:42:20 -0400

    created schemas for orders, products, and users

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,7 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
-import '../products.js'
 
 const Product = ({ product }) => {
     return (
