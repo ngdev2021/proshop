127b9e8f3fcb7e3d7403ad3467ac9d845f4666cc
ngdev2021 <90414221+ngdev2021@users.noreply.github.com>
2023-09-05 16:23:05 -0400

    changed footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <Container>
                 <Row>
-                    <Col className='text-center py-3'><p>Copywrite &copy; {currentYear} ProShop</p></Col>
+                    <Col className='text-center py-3'><p>Copywrite &copy; {currentYear} BlackLight Marketplace</p></Col>
                 </Row>
             </Container>
         </footer>
